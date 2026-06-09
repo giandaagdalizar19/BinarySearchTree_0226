ec01a6af508ad84641472453081a93928b0cac60
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-06-09 10:45:22 +0700

    class node

--- searchtree.cpp
+++ searchtree.cpp
@@ -2,4 +2,18 @@
 #include <string>
 using namespace std;
 
+class node
+{
+public:
+    string info;
+    node *leftchild;
+    node *rightchild;
 
+    //constructor for node class
+    node (string i, node *l, node *r)
+    {
+        info =1;
+        leftchild = l;
+        rightchild = r;
+    }
+}
