6b8bed79dce538523fae11b0246a2950a783c7ac
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-06-09 11:16:41 +0700

    while dan char

--- searchtree.cpp
+++ searchtree.cpp
@@ -125,4 +125,22 @@ void insert(string element)
 };
 
 int main()
-    
+    {
+        binarytree obj;
+
+        while (true)
+        {
+            cout << "\nmenu" << endl;
+            cout << "1. implement insert operation" << endl;
+            cout << "2. implement inorder traversal" << endl;
+            cout << "3. implement preorder traversal" << endl;
+            cout << "4. implement postorder traversal" << endl;
+            cout << "5. exit" << endl;
+            cout << "\nenter your choice: " << endl;
+
+            char ch;
+            cin >> ch;
+
+            cout << endl;
+
+            
